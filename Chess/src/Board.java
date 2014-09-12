e2d9b1c9bb332a06340da102d5b0f63b30737e76
MasterPpv <pritenvora@gmail.com>
2014-09-12 01:19:16 -0500

    Just a quick commit before heading to bed.

--- Chess/src/Board.java
+++ Chess/src/Board.java
@@ -1,10 +1,10 @@
 /**
- * A representation of a chess board. Made up of rows and columns of tiles,
+ * A representation of a chess board. Made up of ranks and files of tiles,
  * each of which may or may not be occupied by a piece.
  * 
  * @author Priten Vora
  *
  */
 public class Board {
-
+	Tile[][] board;
 }
