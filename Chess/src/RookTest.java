c3dfc42244b07bfc364b745f5300b4f532fa1b3b
MasterPpv <pritenvora@gmail.com>
2014-09-19 10:16:51 -0500

    Starting to work on movement of pieces. Still need to work on check and checkmate logic, a GUI, special rules and moves like castling and pawn promotion, and JUnit tests. Very far behind and will probably spend the entirety of this weekend working on this. NOTE: Re-commit because I forgot to add a new file.
    Signed-off-by: MasterPpv <pritenvora@gmail.com>

---
+++ Chess/src/RookTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RookTest {
+
+	@Test
+	public void testCan_move() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCan_move_to() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPotential_moves() {
+		fail("Not yet implemented");
+	}
+
+}
